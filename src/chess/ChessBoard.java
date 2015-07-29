d42036a65e435518b2f63acc4e2788a911455011
Jayapradha V R <jayapradhavr@gmail.com>
2015-07-29 15:06:36 +0530

    Implemented all functions
    displayBoard is not giviing the right output. Should fix it.

--- src/chess/ChessBoard.java
+++ src/chess/ChessBoard.java
@@ -1,12 +1,14 @@
 package chess;
 
-import java.util.HashMap;
+
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ChessBoard {
 
-    Map<String,String> pieces= new HashMap<String,Position>();
-
+    Map<String,Position> pieces = new LinkedHashMap<String,Position>();
+    Map<Position,String> reversed_pieces = new LinkedHashMap<Position,String>();
+    
     ChessBoard(){
         pieces.put("WP1", new Position(1,2));
         pieces.put("WP2", new Position(2,2));
@@ -41,17 +43,49 @@ public class ChessBoard {
         pieces.put("BB2", new Position(6,8));
         pieces.put("BN2", new Position(7,8));
         pieces.put("BR2", new Position(8,8));
+        
+        updateReversedPieces();
     }
     
-    void updateBoard(String piece, Position position, char colour){
+    void updateBoard(String piece, Position position){
         pieces.put(piece, position);
     }
     
-    void displayBoard(){
-        for(int i=8; i>=0; i--){
-            for(int j=1; j<=8; j++){
-                
-            }
+    void updateReversedPieces(){
+        for (Map.Entry<String, Position> piece : pieces.entrySet()) {
+            reversed_pieces.put(piece.getValue(), piece.getKey());
         }
     }
-}
+    
+    void printReversedPieces(){
+        for (Map.Entry<Position, String> item : reversed_pieces.entrySet()) {
+            System.out.println("Position: " + item.getKey().printPosition() + " Piece: " + item.getValue());
+        }
+    }
+
+    void printPieces(){
+        for (Map.Entry<String, Position> item : pieces.entrySet()) {
+            System.out.println("Piece: " + item.getKey() + " Position: " + item.getValue().printPosition());
+        }
+    }    
+    
+    void displayBoard(){
+        updateReversedPieces();
+        for(int i=8; i>=1; i--){
+            for(int j=1; j<=8; j++){
+                Position key = new Position(i,j);
+                String value = reversed_pieces.get(key);
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    Position[] getPositionOfPiece(String piece){
+        Position[] positions = new Position[2];
+        String piece1 = piece + "1";
+        String piece2 = piece + "2";
+        positions[0] = pieces.get(piece1);
+        positions[1] = pieces.get(piece2);
+        return positions;
+    }
